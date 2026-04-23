072030f80fb4bbd801c3d7aa8ecd45faddea840e
Kilián Malý <kilianmaly@gmail.com>
2026-04-23 19:48:04 +0200

    Renamed function to raw_ingest_flow()

--- orchestration/flows/pipeline.py
+++ orchestration/flows/pipeline.py
@@ -1,13 +1,16 @@
-from orchestration.tasks.extract import get_open_positions
+from orchestration.tasks.extract import get_open_positions, get_orders_history
 from orchestration.tasks.load import load_to_db
 
 from prefect import flow
 
 @flow(name="etl-pipeline")
-def etl_pipeline():
-    df_extract = get_open_positions()
-    transform = load_to_db(df_extract)
+def raw_ingestion_flow():
+    positions = get_open_positions()
+    load_to_db(positions, "raw_open_positions")
+
+    orders = get_orders_history()
+    load_to_db(orders, "raw_orders_history")
 
 
 if __name__ == "__main__":
-    etl_pipeline()
+    raw_ingestion_flow()
