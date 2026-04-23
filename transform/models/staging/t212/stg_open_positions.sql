2b97875641a49b9eb9722b5125c471c91f496bb4
Kilián Malý <kilianmaly@gmail.com>
2026-04-23 21:34:01 +0200

    Defined staging models and wrote tests

---
+++ transform/models/staging/t212/stg_open_positions.sql
@@ -0,0 +1,33 @@
+{{ config(materialized='view') }}
+
+with source as (
+    select * from {{ source('t212', 'raw_open_positions') }}
+)
+
+,flattened as (
+select
+    extract_timestamp,
+    record_id ticker,
+
+    nullif(json_value(payload, '$.instrument.ticker'), '') instrument_ticker,
+    nullif(json_value(payload, '$.instrument.name'), '') instrument_name,
+    nullif(json_value(payload, '$.instrument.isin'), '') isin,
+    nullif(json_value(payload, '$.instrument.currency'), '') instrument_currency,
+    cast(json_value(payload, '$.createdAt') as timestamp) position_created_at,
+
+    cast(json_value(payload, '$.quantity') as float64) quantity,
+    cast(json_value(payload, '$.quantityAvailableForTrading') as float64) quantity_available_for_trading,
+    cast(json_value(payload, '$.quantityInPies') as float64) quantity_in_pies,
+
+    cast(json_value(payload, '$.currentPrice') as float64) current_price,
+    cast(json_value(payload, '$.averagePricePaid') as float64) average_price_paid,
+
+    nullif(json_value(payload, '$.walletImpact.currency'), '') wallet_currency,
+    cast(json_value(payload, '$.walletImpact.totalCost') as float64) total_cost,
+    cast(json_value(payload, '$.walletImpact.currentValue') as float64) current_value,
+    cast(json_value(payload, '$.walletImpact.unrealizedProfitLoss') as float64) unrealized_profit_loss,
+    cast(json_value(payload, '$.walletImpact.fxImpact') as float64) fx_impact
+from source
+)
+
+SELECT * from flattened
---
+++ transform/models/staging/t212/stg_orders_history.sql
@@ -0,0 +1,39 @@
+{{ config(materialized='view') }}
+
+with source as (
+    select * from {{ source('t212', 'raw_orders_history') }}
+)
+
+,flattened as (
+select
+    extract_timestamp,
+    record_id order_id,
+    record_created_at created_at,
+
+    nullif(json_value(payload, '$.order.strategy'), '') strategy,
+    nullif(json_value(payload, '$.order.ticker'), '') ticker,
+    nullif(json_value(payload, '$.order.status'), '') status,
+    nullif(json_value(payload, '$.order.currency'), '') currency,
+    nullif(json_value(payload, '$.order.initiatedFrom'), '') initiated_from,
+    nullif(json_value(payload, '$.order.side'), '') side,
+    cast(json_value(payload, '$.order.value') as float64) order_value,
+    cast(json_value(payload, '$.order.filledValue') as float64) filled_value,
+    cast(json_value(payload, '$.order.extendedHours') as bool) extended_hours,
+
+    nullif(json_value(payload, '$.order.instrument.isin'), '') isin,
+    nullif(json_value(payload, '$.order.instrument.currency'), '') instrument_currency,
+
+    cast(json_value(payload, '$.fill.quantity') as float64) filled_quantity,
+    cast(json_value(payload, '$.fill.price') as float64) fill_price,
+    nullif(json_value(payload, '$.fill.type'), '') fill_type,
+    nullif(json_value(payload, '$.fill.tradingMethod'), '') trading_method,
+    cast(json_value(payload, '$.fill.filledAt') as timestamp) filled_at,
+
+    nullif(json_value(payload, '$.fill.walletImpact.currency'), '') wallet_currency,
+    cast(json_value(payload, '$.fill.walletImpact.netValue') as float64) net_value,
+    cast(json_value(payload, '$.fill.walletImpact.fxRate') as float64) fx_rate,
+    cast(json_value(payload, '$.fill.walletImpact.taxes') as float64) taxes
+from `t212.raw_orders_history`
+)
+
+SELECT * from flattened
