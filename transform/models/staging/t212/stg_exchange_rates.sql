d0b382212c9fc84d2c6ed55dfef930ebf314e74c
Kilián Malý <kilianmaly@gmail.com>
2026-04-26 11:48:07 +0200

    Added dependencies.yml and the new model

---
+++ transform/models/staging/t212/stg_exchange_rates.sql
@@ -0,0 +1,34 @@
+{{ config(materialized='view') }}
+
+with source as (
+    select * from {{ source('t212', 'raw_exchange_rates') }}
+),
+
+unnested as (
+    select
+        record_id,
+        base,
+        extract_timestamp,
+        currency,
+        rate
+    from source,
+    unnest([
+        struct('USD' as currency, payload.USD as rate),
+        struct('GBP' as currency, payload.GBP as rate),
+        struct('CZK' as currency, payload.CZK as rate),
+        struct('CHF' as currency, payload.CHF as rate)
+    ])
+),
+
+final as (
+    select
+        {{ dbt_utils.generate_surrogate_key(['record_id', 'currency']) }} as surrogate_key,
+        record_id as date,
+        currency,
+        base,
+        cast(rate as float64) as rate,
+        extract_timestamp
+    from unnested
+)
+
+select * from final
