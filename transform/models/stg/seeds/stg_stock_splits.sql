6979903e518e3c26e42660c13d9924f90edd71bb
Kilián Malý <kilianmaly@gmail.com>
2026-04-27 22:11:03 +0200

    Created stg_stock_split model

---
+++ transform/models/stg/seeds/stg_stock_splits.sql
@@ -0,0 +1,17 @@
+{{ config(materialized='view') }}
+
+with source as (
+    select * from {{ source('t212_seeds', 'raw_stock_splits') }}
+)
+
+,final as (
+select
+  {{ dbt_utils.generate_surrogate_key(['ticker', 'date']) }} as surrogate_key,
+  ticker,
+  date,
+  ratio_old,
+  ratio_new
+from source
+)
+
+select * from final
