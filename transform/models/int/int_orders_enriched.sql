0c62775d7219d229c050c27bb5602913d14f8d09
Kilián Malý <kilianmaly@gmail.com>
2026-05-02 11:55:04 +0200

    Added deduplication cte for ETFs operations that were divided into two parts

--- transform/models/int/int_orders_enriched.sql
+++ transform/models/int/int_orders_enriched.sql
@@ -7,11 +7,32 @@ where true
     and fill_type != 'STOCK_SPLIT'
 )
 
+,dedup_orders as (
+select
+	order_id,
+	isin,
+	side,
+	MIN(created_at) created_at,
+	SUM(filled_quantity) filled_quantity,
+	-- WEIGHTED AVERAGE FILL PRICE
+	SUM(fill_price * filled_quantity) / SUM(filled_quantity) fill_price,
+	instrument_currency,
+	SUM(net_value) net_value,
+	currency,
+	-- WEIGHTED AVERAGE FX RATE
+	SUM(fx_rate * filled_quantity) / SUM(filled_quantity) fx_rate,
+	SUM(currency_conversion_fee) currency_conversion_fee,
+	MAX(filled_at) filled_at,
+	MAX(extract_timestamp) extract_timestamp
+from orders
+group by order_id, isin, side, instrument_currency, currency
+)
+
 ,orders_with_split_factor as (
 select
 	o.order_id,
 	coalesce(sum(ratio_old / ratio_new), 1.0) as split_adjustment_factor
-from orders o
+from dedup_orders o
 left join {{ ref('stg_stock_splits') }} s
 	on s.isin = o.isin
 	and s.date > cast(o.filled_at as date)
@@ -36,7 +57,7 @@ select
     o.fill_price / adj.split_adjustment_factor as fill_price_split_adjusted,
 	o.filled_at,
 	o.extract_timestamp
-from orders o
+from dedup_orders o
 left join orders_with_split_factor adj on o.order_id = adj.order_id
 )
 
