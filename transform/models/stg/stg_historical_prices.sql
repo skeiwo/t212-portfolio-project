3b83a5cccbc84a507ae1456004efe9af029d2a11
Kilián Malý <kilianmaly@gmail.com>
2026-05-02 19:35:08 +0200

    Added stg_historical_prices.sql model

---
+++ transform/models/stg/stg_historical_prices.sql
@@ -0,0 +1,22 @@
+{{ config(materialized='view') }}
+
+with source as (
+    select * from {{ source('t212_raw', 'raw_historical_prices') }}
+),
+
+flattened as (
+    select
+        extract_timestamp,
+        ticker,
+        record_date,
+
+        cast(json_value(payload, '$.open') as float64) open,
+        cast(json_value(payload, '$.high') as float64) high,
+        cast(json_value(payload, '$.low') as float64) low,
+        cast(json_value(payload, '$.close') as float64) close,
+        cast(json_value(payload, '$.adj_close') as float64) adj_close,
+        cast(json_value(payload, '$.volume') as int64) volume
+    from source
+)
+
+select * from flattened
