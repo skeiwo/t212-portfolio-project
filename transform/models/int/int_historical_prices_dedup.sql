1ae27ca6b4fb4b3b1cdddbaf24f5902629030198
Kilián Malý <kilianmaly@gmail.com>
2026-05-08 21:48:42 +0200

    Added deduplication

---
+++ transform/models/int/int_historical_prices_dedup.sql
@@ -0,0 +1,34 @@
+{{ config(materialized='table') }}
+
+with prices as (
+    select * from {{ ref('stg_historical_prices') }}
+)
+
+,prices_deduped as (
+select * except(extract_rank)
+from (
+	select
+		*,
+		row_number() over (partition by record_id order by extract_timestamp desc) extract_rank
+	from prices
+)
+where extract_rank = 1
+)
+
+,final as (
+select
+	record_id,
+	isin,
+	ticker,
+	record_date,
+	open,
+	high,
+	low,
+	close,
+	adj_close,
+	volume,
+	extract_timestamp
+from prices_deduped
+)
+
+select * from final
